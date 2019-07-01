c6c004559578c94586197807226bd1a6b4bf5f7c
SalimovaNellia <salimova.nellia@gmail.com>
2019-07-01 14:38:50 +0300

    Updates

---
+++ precache-manifest.cafb6e5f108885b2d876e35271214da9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d40121a56a8fc71c6da960008e7e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9561c95954d8556b7ab1",
+    "url": "/static/js/2.484aea99.chunk.js"
+  },
+  {
+    "revision": "0d28cf3bc419622b8cf2",
+    "url": "/static/js/main.5d383988.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
